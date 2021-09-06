1557d34c58d86f04ca5ebebc948c30a36c992b4d
user <>
2021-09-06 23:42:34 +0900

    first commit

---
+++ UberEatsLike/Models/RestaurantsModel.swift
@@ -0,0 +1,8 @@
+//
+//  RestaurantsModel.swift
+//  UberEatsLike
+//
+//  Created by user on 2021/09/04.
+//
+
+import Foundation
---
+++ UberEatsLike/RestaurantTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RestaurantTableViewCell.swift
+//  UberEatsLike
+//
+//  Created by user on 2021/09/05.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
